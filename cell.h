41a8827c5f73aee8574cb8fff0bac8c3673a3f76
= <Kinneyanick@gmail.com>
2018-10-11 18:29:01 -0500

    Added more to grid

--- cell.h
+++ cell.h
@@ -2,22 +2,22 @@
 #define cellIncluded
 
 #include <vector>
+namespace GCA{
+    class Cell {
+        public:
+            Cell(int xPosition, int yPosition, int value);
+            int getValue();
+            bool getActive();
+            std::vector<int>position getPosition();
+            void setValue(int value);
+            void setActive(bool active);
+            void setPosition(int x, int y);
+            void setPosition(std::vector<int> position);
 
-class Cell {
-    public:
-        Cell(int xPosition, int yPosition, int value);
-        int getValue();
-        bool getActive();
-        std::vector<int>position getPosition();
-        void setValue(int value);
-        void setActive(bool active);
-        void setPosition(int x, int y);
-        void setPosition(std::vector<int> position);
-
-    protected:
-        std::vector<int> position;
-        bool active;
-        int value;
-};
-
+        protected:
+            std::vector<int> position;
+            bool active;
+            int value;
+    };
+}
 #endif
--- grid.h
+++ grid.h
@@ -3,15 +3,34 @@
 
 #include <vector>
 #include "cell.h"
+namespace GCA{
+    class Grid {
+        public:
+            //Constructor
+            Grid(int width, int height);
+            
+            //Operator overload to allow for requesting individual cells
+            Cell* operator[](int y, int x);
 
-class Grid {
-    public:
-        Grid(int width, int height);
-        
+            //Get and Set methods
+            std::vector<Cell*> getCells();
+            
+            Cell* getCell(int index);
+            Cell* getCell(int y, int x);
+            
+            int getWidth();
+            int getHeight();
+            int getSize();
+            int size();
 
-    protected:
-        std::vector<Cell*> cells;
+            //Iterators
+            iterator begin();
+            iterator end();
 
-};
+        protected:
+            std::vector<Cell*> cells;
+            int width, height, size;
 
+    };
+}
 #endif
